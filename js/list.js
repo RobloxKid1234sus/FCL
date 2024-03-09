4bea07dd9518c013f24891c8e928aa94a31c720a
RobloxKid1234sus <147947539+RobloxKid1234sus@users.noreply.github.com>
2024-03-09 13:54:47 -0500

    Update list.js

--- js/list.js
+++ js/list.js
@@ -1,22 +1,44 @@
-/*=============================================================================*/
+ const d = {
+  list: [
+    /*=============================================================================*/
+    {
+      vids: [
+        {
+          user: "toon",
+          link: "https://www.youtube.com/watch?v=vf87NTxeHNI",
+          percent: 100,
+          hz: "360hz",
+        },
+        {
+          user: "Willow",
+          link: "https://www.youtube.com/watch?v=djyPOYUANjs",
+          percent: 100,
+          hz: "240hz",
+        },
+        {
+          user: "nybia",
+          link: "https://www.youtube.com/watch?v=QVmLpF2_Hok",
+          percent: 100,
+          hz: "360hz",
+        },
+        {
+          user: "Draven",
+          link: "https://youtu.be/QjM6gsxSGS8",
+          percent: 100,
+          hz: "360hz",
+        },
 {
-	"vids": [
-		{
-			"user": "ImpossibleLevel",
-			"link": "",
-			"percent": (100),
-			"hz": "360"
-		},  
-	],        
-	name: "Solar Wind",
-	author: "ImpossibleLevel",
-	more: "none",
-	desc: "I honestly dont know",
-	id: 544,
-	pass: 0,
-	percentToQualify: 100,
-	verificationVid: "vidLink",
-	phacked: false,
-	hacked: false
-},
+          user: "N1x",
+          link: "https://www.youtube.com/watch?v=4fzUBzbstCs",
+          percent: 100,
+          hz: "360hz",
+        },
+      ],
+      name: "Shitty SADOMASOCHISM",
+      author: "Jasker [Coakmuffer]",
+      id: 81835814,
+      percentToQualify: 100,
+      verificationVid: "https://www.youtube.com/watch?v=ydYqELe-uA8",
+    },
+        /*=============================================================================*/
 /*=============================================================================*/
